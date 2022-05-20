ebe715fda95387196b64a9d205a930ac8094fc61
Manish <manish.kini@magitt.com>
2022-05-20 14:46:19 +0530

    models views routes updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const expres = require('express');
 
 const router = expres.Router();
+const homeController = require('../controllers/homeController');
 
 // Different routes
+router.get('/', homeController.home);
 
 module.exports = router;
